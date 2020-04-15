29f1bd39a002737af658d0a86626a89481f51601
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-15 02:33:07 +0000

    added migration files

---
+++ db/migrate/02_new_item.rb
@@ -0,0 +1,11 @@
+class Items < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string  :item_name
+      t.string  :character
+      t.string  :description
+      t.date  :date
+      t.integer  :user_id
+    end
+  end
+end
