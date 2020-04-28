87e030c633914ac09ccbd132e1f273bbcc328f85
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-28 21:42:58 +0000

    added code skeleton to app_controller

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,38 @@
 require './config/environment'
 
 class AppController < Sinatra::Base
+  #skeleton put in place
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    #Need to learn how to set session_secret
+    set :session_secret, "changeme"
+  end
+
+  get "/" do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :index
+    end
+  end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id])
+    end
+
+    def authorized?(id)
+      current_user.id == id
+    end
+  end
+
 
 end
