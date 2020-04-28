3b621d29da3914b2c400cb6fb88d8da34bf94f17
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-28 21:47:07 +0000

    controllers skeleton added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,53 @@
 class UserController < AppController
 
+  #added code skeleton
+ get '/signup' do
+    if !session[:user_id]
+      erb :'users/create_user'
+    else
+      redirect to('/expenses')
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if !@user.save
+      @errors = @user.errors.full_messages
+      erb :'users/create_user'
+    else
+      session[:user_id] = @user.id
+      redirect to('/expenses')
+    end
+  end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect to('/expenses')
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to('/expenses')
+    else
+      @errors = "Invalid username or password."
+      erb :'users/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      @user = current_user
+      @user = nil
+      session.destroy
+      redirect to('/')
+    else
+      rediect to('/')
+    end
+  end
+
 end
