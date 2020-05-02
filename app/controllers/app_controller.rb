12a01f47937c72c141c651800f960cdab7df0b01
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-02 18:13:09 +0000

    deleted authorization

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -7,14 +7,14 @@ class AppController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "timetoduel" #ENV.fetch('SESSION_SECRET')
+    set :session_secret, "themightynein" #ENV.fetch('SESSION_SECRET')
   end
 
   get "/" do
     if logged_in?
       redirect "/users/#{current_user.id}"
     else
-      erb :index
+      erb :welcome
     end
   end
 
@@ -27,11 +27,4 @@ class AppController < Sinatra::Base
     def current_user
       @current_user ||= User.find(session[:user_id])
     end
-
-    def authorized?(id)
-      current_user.id == id
-    end
-  end
-
-
 end
