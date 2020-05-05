ef059597c14eab9949392263ed5005f8b1a7c3dc
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-05 15:10:15 +0000

    fixed gemfile, added migration folder

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -7,16 +7,15 @@ class AppController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "themightynein" #ENV.fetch('SESSION_SECRET')
+    set :session_secret, "timetoduel" #ENV.fetch('SESSION_SECRET')
   end
 
   get "/" do
     if logged_in?
       redirect "/users/#{current_user.id}"
     else
-      erb :welcome
+      erb :index
     end
-  end
 
 
   helpers do
@@ -27,4 +26,10 @@ class AppController < Sinatra::Base
     def current_user
       @current_user ||= User.find(session[:user_id])
     end
+
+    def authorized?(id)
+      current_user.id == id
+    end
+  end
+end
 end
