204e0b4b466542e7147323529982084d5dfbaf1f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-05 15:25:54 +0000

    deleted spare file, changed rakefile around

--- Rakefile
+++ Rakefile
@@ -1,7 +1,9 @@
-ENV["SINATRA_ENV"] ||= "development"
-
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
+
+def reload
+  load_all 'app/models'
+end
 
 
 
---
+++ db/migrate/02_items.rb
@@ -0,0 +1,11 @@
+class Items < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string  :title
+      t.string  :description
+      t.date  :date
+      t.string  :character
+      t.integer  :user_id
+    end
+  end
+end
