734e3aafec2dc50b5050c9401eb5d86b96c2bfca
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-09 20:07:17 +0000

    started again from gemfile

--- app/models/item.rb
+++
@@ -1,7 +0,0 @@
-class Item < ActiveRecord::Base
-
-  belongs_to :user
-
-  validates :title, :character, :description, :date, presence: true
-
-end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-
-  has_many :items
-  validates :username, :email, presence: true
-
-  has_secure_password
-
-end
